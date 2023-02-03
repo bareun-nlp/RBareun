c09cfbc0ab5a21d0d1e222b60830c8dea671d72f
hunbL <hunbl@baikal.ai>
2023-02-03 11:15:55 +0900

    update docs & clean proj.

--- R/utils.R
+++ R/utils.R
@@ -1,8 +1,11 @@
-#' Provides a list of logging functions to be passed to gRPC server
-#'
-#' You may want to override this with custom functions, eg registering the gRPC service in Hashicorp's consul.io or similar after \code{server_start} etc.
-#' @return \code{list}
-#' @importFrom futile.logger flog.trace flog.debug flog.info
+# Provides a list of logging functions to be passed to gRPC server
+#
+# You may want to override this with custom functions,
+# eg registering the gRPC service in Hashicorp's consul.io
+# or similar after \code{server_start} etc.
+#
+# @return \code{list}
+# @importFrom futile.logger flog.trace flog.debug flog.info
 grpc_default_hooks <- function() {
   list(
     server_create = function(params) {
