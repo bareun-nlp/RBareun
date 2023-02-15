a66aa4a24c7a6d0f031a864c03e33126b000f5f6
hunbL <hunbl@naver.com>
2023-02-15 17:11:49 +0900

    Update zzz.R

--- R/zzz.R
+++ R/zzz.R
@@ -1,6 +1,6 @@
 .onAttach <- function(libname, pkgName) {
     if (interactive()) {
         packageStartupMessage("RBareun ", packageVersion("bareun"),
-                              " using Bareun/1.7")
+                              " using Bareun/1.8")
     }
 }
