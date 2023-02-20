b0e0745f8b0c6603c5b352ef1169b3aa34add0bd
hunbL <hunbl@baikal.ai>
2023-02-20 15:14:40 +0900

    upd: version bareun 2.0

--- R/zzz.R
+++ R/zzz.R
@@ -1,6 +1,6 @@
 .onAttach <- function(libname, pkgName) {
     if (interactive()) {
         packageStartupMessage("RBareun ", packageVersion("bareun"),
-                              " using Bareun/1.8")
+                              " using Bareun/2.0")
     }
 }
